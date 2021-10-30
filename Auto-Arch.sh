81dbe0fe720938c268c7f210f2dd27a9383b7903
[Anthony] [Picciano] <[anthony.a.picciano@gmail.com]>
2021-10-30 02:50:10 -0500

    Update install script.

--- Auto-Arch.sh
+++ Auto-Arch.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+Auto-Arch#!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
-    source /mnt/root/ArchTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    arch-chroot /mnt /root/Auto-Arch/1-setup.sh
+    source /mnt/root/Auto-Arch/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/Auto-Arch/2-user.sh
+    arch-chroot /mnt /root/Auto-Arch/3-post-setup.sh
